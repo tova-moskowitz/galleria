9196fb4ffd326cecc40d085be16094c38ff2ffe5
= <=>
2023-10-09 00:07:55 -0400

    add foundation of header component- logo and <hr>

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="galleria-header">
+      <img src="./assets/shared/logo.svg" />
+      <hr />
+    </div>
+  );
+}
+
+export default Header;
