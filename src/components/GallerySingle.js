93e9a6270857078f4828888f1953f94f2ebe8703
= <=>
2023-10-10 16:43:31 -0400

    move some component files directly into components folder

---
+++ src/components/GallerySingle.js
@@ -0,0 +1,34 @@
+import React from "react";
+import data from "../data/data.json";
+import "../styles/gallery.scss";
+
+function gallerySingle() {
+  const clickOpenModal = (e) => {
+    console.log(e);
+  };
+
+  return (
+    <div className="single">
+      <div className="main-content">
+        <button onClick={clickOpenModal} className="modal-open">
+          <img src={"/assets/shared/icon-view-image.svg"} />
+          VIEW IMAGE
+        </button>
+        <div className="main-image">
+          <img className="painting-image" src={data[0].images.hero.large} />
+        </div>
+        <div className="title-and-artist-box">
+          <div className="painting-title">{data[0].name}</div>
+          <div className="artist-name">{data[0].artist.name}</div>
+        </div>
+        <div className="date">{data[0].year}</div>
+        <div className="description">{data[0].description}</div>
+        <img className="artist-image" src={data[0].artist.image} />
+        <a href={data[0].source} className="source">
+          GO TO SOURCE
+        </a>
+      </div>
+    </div>
+  );
+}
+export default gallerySingle;
