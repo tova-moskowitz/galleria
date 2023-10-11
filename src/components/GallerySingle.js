6ffe4b9067f9e9190a3efa04cfe041494e411202
= <=>
2023-10-11 16:05:23 -0400

    add alt to images

--- src/components/GallerySingle.js
+++ src/components/GallerySingle.js
@@ -8,22 +8,29 @@ function gallerySingle() {
   };
 
   return (
-    <div className="single">
+    <div className="gallery-single-wrapper">
       <div className="main-content">
         <button onClick={clickOpenModal} className="modal-open">
           <img src={"/assets/shared/icon-view-image.svg"} />
           VIEW IMAGE
         </button>
         <div className="main-image">
-          <img className="painting-image" src={data[0].images.hero.large} />
+          <img
+            className="painting-image"
+            src={data[0].images.hero.large}
+            alt={data[0].name}
+          />
         </div>
         <div className="title-and-artist-box">
           <div className="painting-title">{data[0].name}</div>
           <div className="artist-name">{data[0].artist.name}</div>
         </div>
+        <img className="artist-image" src={data[0].artist.image} />
+      </div>
+
+      <div className="date-and-description-box">
         <div className="date">{data[0].year}</div>
         <div className="description">{data[0].description}</div>
-        <img className="artist-image" src={data[0].artist.image} />
         <a href={data[0].source} className="source">
           GO TO SOURCE
         </a>
