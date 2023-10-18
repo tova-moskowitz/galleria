fc6100fb22f28712819f88c9e1fae71a426d72b5
= <=>
2023-10-18 17:43:58 -0400

    added ids to paintings so can navigate to paintings on homepage

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -2,22 +2,14 @@ import React from "react";
 import data from "../data/data.json";
 import "../styles/gallery.scss";
 import Footer from "./Footer.js";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useNavigate,
-  useParams,
-  Link,
-} from "react-router-dom";
+import { useParams } from "react-router-dom";
 function Slideshow({ pageNumber, clickBack, clickNext }) {
   const clickOpenModal = (e) => {};
+  const { paintingId } = useParams();
 
-  const { id } = useParams();
-  console.log(id);
-
+  const painting = data.find((painting) => painting.id === +paintingId);
   return (
-    <div data-page-number={id} className="gallery-single-wrapper">
+    <div className="gallery-single-wrapper">
       <div className="main-content">
         <button onClick={clickOpenModal} className="modal-open">
           <img src={"/assets/shared/icon-view-image.svg"} />
@@ -25,29 +17,26 @@ function Slideshow({ pageNumber, clickBack, clickNext }) {
         </button>
         <div className="images">
           <div className="painting-image">
-            <img
-              src={data[pageNumber].images.hero.large}
-              alt={data[pageNumber].name}
-            />
+            <img src={`../${painting.images.hero.large}`} alt={painting.name} />
           </div>
         </div>
 
         <div className="title-and-artist-box">
-          <div className="painting-title">{data[pageNumber].name}</div>
-          <div className="artist-name">{data[pageNumber].artist.name}</div>
+          <div className="painting-title">{painting.name}</div>
+          <div className="artist-name">{painting.artist.name}</div>
           <div className="artist-image">
             <img
-              src={data[pageNumber].artist.image}
-              alt={data[pageNumber].artist.name}
+              src={`../${painting.artist.image}`}
+              alt={painting.artist.name}
             />
           </div>
         </div>
       </div>
 
       <div className="date-and-description-box">
-        <div className="date">{data[pageNumber].year}</div>
-        <div className="description">{data[pageNumber].description}</div>
-        <a href={data[pageNumber].source} className="source">
+        <div className="date">{painting.year}</div>
+        <div className="description">{painting.description}</div>
+        <a href={painting.source} className="source">
           GO TO SOURCE
         </a>
       </div>
