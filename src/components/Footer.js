99c09990a5deda610ad09e919e6ad44e917071c7
= <=>
2023-10-19 13:30:59 -0400

    disable next and back buttons when get to beginning or end of slideshow

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,26 @@
-import React from "react";
+import Reac, { useState } from "react";
 import "../styles/footer.scss";
 import data from "../data/data.json";
 import { useParams, Link } from "react-router-dom";
 
 function Footer() {
+  const [disableBack, setDisableBack] = useState("");
+  const [disableNext, setDisableNext] = useState("");
   const { paintingId } = useParams();
+
+  const onClickBack = () => {
+    if (+paintingId === 1) {
+      setDisableBack("disabled");
+    }
+    setDisableNext("");
+  };
+
+  const onClickNext = () => {
+    if (+paintingId === data.length - 2) {
+      setDisableNext("disabled");
+    }
+    setDisableBack("");
+  };
 
   return (
     <div className="galleria-footer">
@@ -14,8 +30,14 @@ function Footer() {
         <div className="artist-name">{data[paintingId].artist.name}</div>
       </div>
       <div className="back-next-buttons">
-        <Link to={`/slideshow/${paintingId > 0 ? +paintingId - 1 : 0}`}>
-          <button className="back-button">
+        <Link
+          to={`/slideshow/${paintingId > 0 ? +paintingId - 1 : paintingId}`}
+        >
+          <button
+            onClick={onClickBack}
+            disabled={disableBack}
+            className="back-button"
+          >
             <img
               src="../assets/shared/icon-back-button.svg"
               alt="back-button"
@@ -24,10 +46,14 @@ function Footer() {
         </Link>
         <Link
           to={`/slideshow/${
-            paintingId < data.length - 1 ? +paintingId + 1 : data.length - 1
+            paintingId < data.length - 1 ? +paintingId + 1 : paintingId
           }`}
         >
-          <button className="next-button">
+          <button
+            onClick={onClickNext}
+            disabled={disableNext}
+            className="next-button"
+          >
             <img
               src="../assets/shared/icon-next-button.svg"
               alt="next button"
