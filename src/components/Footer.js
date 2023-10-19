fa3f69547fb4afffdcfe409d766e7bb433f0b725
= <=>
2023-10-18 20:48:30 -0400

    footer works after refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/footer.scss";
 import data from "../data/data.json";
+import { useParams, Link } from "react-router-dom";
 
-function Footer({ clickBack, clickNext, pageNumber }) {
+function Footer() {
+  const { paintingId } = useParams();
+
   return (
     <div className="galleria-footer">
       <hr />
       <div className="painting-details">
-        <div className="painting-name">{data[pageNumber].name}</div>
-        <div className="artist-name">{data[pageNumber].artist.name}</div>
+        <div className="painting-name">{data[paintingId].name}</div>
+        <div className="artist-name">{data[paintingId].artist.name}</div>
       </div>
       <div className="back-next-buttons">
-        <button
-          onClick={clickBack}
-          data-page={pageNumber}
-          className="back-button"
+        <Link to={`/slideshow/${paintingId > 1 ? +paintingId - 1 : 1}`}>
+          <button className="back-button">
+            <img
+              src="../assets/shared/icon-back-button.svg"
+              alt="back-button"
+            />
+          </button>
+        </Link>
+        <Link
+          to={`/slideshow/${
+            paintingId < data.length ? +paintingId + 1 : data.length
+          }`}
         >
-          <img src="./assets/shared/icon-back-button.svg" alt="back-button" />
-        </button>
-        <button
-          onClick={clickNext}
-          data-page={pageNumber}
-          className="next-button"
-        >
-          <img src="./assets/shared/icon-next-button.svg" alt="next button" />
-        </button>
+          <button className="next-button">
+            <img
+              src="../assets/shared/icon-next-button.svg"
+              alt="next button"
+            />
+          </button>
+        </Link>
       </div>
     </div>
   );
