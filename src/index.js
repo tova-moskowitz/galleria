7aedfb7657c20e4f432116c8cdb14034697c8178
= <=>
2023-10-21 13:12:28 -0400

    hashrouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   // </React.StrictMode>
 );
