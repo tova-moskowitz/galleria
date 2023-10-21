6c8893b3ddca9bffee4e855bee07242aada837a4
= <=>
2023-10-20 21:08:58 -0400

    trying to fix images not showing on slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,40 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import data from "../data/data.json";
 import "../styles/gallery.scss";
 import Footer from "./Footer.js";
 import { useParams } from "react-router-dom";
-import Modal from "./Modal.js";
-
 function Slideshow() {
-  const [modalOpen, setModalOpen] = useState(false);
-
+  const clickOpenModal = (e) => {};
   const { paintingId } = useParams();
   const painting = data.find((painting) => painting.id === +paintingId);
 
   return (
     <>
       <div className="gallery-single-wrapper">
-        {modalOpen && <Modal painting={painting} setModalOpen={setModalOpen} />}
         <div className="main-content">
-          <button
-            onClick={() => {
-              setModalOpen(true);
-            }}
-            className="modal-open"
-          >
+          <button onClick={clickOpenModal} className="modal-open">
             <img src={"/assets/shared/icon-view-image.svg"} />
             VIEW IMAGE
           </button>
           <div className="images">
             <div className="painting-image">
-              <img src={painting.images.hero.large} alt={painting.name} />
+              <img
+                src="./assets/starry-night/gallery.jpg"
+                alt={painting.name}
+              />
             </div>
           </div>
           <div className="title-and-artist-box">
             <div className="painting-title">{painting.name}</div>
             <div className="artist-name">{painting.artist.name}</div>
             <div className="artist-image">
-              <img src={painting.artist.image} alt={painting.artist.name} />
+              <img
+                src={`.${painting.artist.image}`}
+                alt={painting.artist.name}
+              />
             </div>
           </div>
         </div>
